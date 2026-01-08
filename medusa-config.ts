a7907df39bbd6e2582a9780a57ced58a27251ee4
jl-23929 <james@lollback.com>
2026-01-08 22:02:55 +1000

    updates for dokploy

--- medusa-config.ts
+++ medusa-config.ts
@@ -16,5 +16,31 @@ module.exports = defineConfig({
       ssl: false,
       sslmode: "disable",
     },
-  }
+  },
+  admin: {
+    vite: (config) => {
+      return {
+        ...config,
+        server: {
+          ...config.server,
+          host: "0.0.0.0",
+          // Allow all hosts when running in Docker (development mode)
+          // In production, this should be more restrictive
+          allowedHosts: [
+            "localhost",
+            ".localhost",
+            "127.0.0.1",
+          ],
+          hmr: {
+            ...config.server?.hmr,
+            // HMR websocket port inside container
+            port: 5173, 
+            // Port browser connects to (exposed in docker-compose.yml)
+            clientPort: 5173,
+          },
+        },
+      }
+    },
+  },
+
 })
